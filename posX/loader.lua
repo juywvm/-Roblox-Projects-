0d043097034a12e95a072c4257b8236852bc3ed8
TheyCallMeBatus <105020741+juywvm@users.noreply.github.com>
2022-12-27 19:42:10 +0300

    Update loader.lua

--- posX/loader.lua
+++ posX/loader.lua
@@ -1,1 +1,10 @@
-eeee
+--[[
+
+Hello, welcome to posX loader. Do not use raw if you want to execute everytime latest version of posX.
+
+
+
+
+]]
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/juywvm/-Roblox-Projects-/main/posX/main.lua'),true))()
